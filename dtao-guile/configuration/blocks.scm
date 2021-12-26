938b95c81b31c6a616d2dd0669009bc5533f74f3
Fredrik Engstrand <fredrik@engstrand.nu>
2021-12-26 09:37:34 +0100

    chore: rename dtao-guile-block to dtao-block

--- dtao-guile/configuration/blocks.scm
+++ dtao-guile/configuration/blocks.scm
@@ -1,23 +1,22 @@
-(define-module (dtao-guile blocks)
+(define-module (dtao-guile configuration blocks)
                #:use-module (gnu services configuration)
                #:export (
-                         dtao-guile-block
-                         dtao-guile-block?
-                         <dtao-guile-block>
-                         dtao-guile-block-render
-                         ))
+                         dtao-block
+                         dtao-block?
+                         <dtao-block>
+                         dtao-block-render))
 
 ;; Number of arguments required by the block renderer procedure.
 (define %block-renderer-arguments 2)
 
 (define (block-renderer? proc)
-  (or (eq? x #f)
+  (or (eq? proc #f)
       (and (procedure? proc)
            (eq? (car (procedure-minimum-arity proc))
                 %block-renderer-arguments))))
 
 (define-configuration
-  dtao-guile-block
+  dtao-block
   (render
     (block-renderer #f)
     "Block rendering procedure.")
